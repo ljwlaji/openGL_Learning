739d8c867ef83a735746f96588be595079b483cc
ljw <602809934@qq.com>
2021-12-03 22:11:10 +0800

    Initial Commit

---
+++ gll/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gll
+//
+//  Created by lijingwei on 2021/12/3.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
