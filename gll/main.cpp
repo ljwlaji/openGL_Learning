599e8e36a7f08b01b14444e164e07e9b76aa200f
ljw <602809934@qq.com>
2021-12-03 23:00:12 +0800

    三角形

--- gll/main.cpp
+++ gll/main.cpp
@@ -6,9 +6,35 @@
 //
 
 #include <iostream>
+#include <../includes/GLFW/glfw3.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    if (!glfwInit())
+        return 0;
+//    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+//    glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Wave Simulation", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return 0;
+    }
+    
+    glfwMakeContextCurrent(window);
+    
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
     std::cout << "Hello, World!\n";
     return 0;
 }
