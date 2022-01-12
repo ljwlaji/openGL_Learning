2a7cf51a41ee77b4171b51a1f7a363943f76699c
ljw <602809934@qq.com>
2022-01-12 13:58:25 +0800

    暂存

--- gll/GLWindow.hpp
+++ gll/GLWindow.hpp
@@ -15,18 +15,35 @@
 #include <../includes/glm/gtc/matrix_transform.hpp>
 #include "Sprite.hpp"
 #include "IndexBuffer.hpp"
-
-
+#include <list>
+#include <array>
 #define MAX_RECT_NUMBER 10000
 #define MAX_QUAD_COMMAND 40000
 #define MAX_INDEX_NUMBER 60000
 
-
 typedef struct QuadCommand {
+    QuadCommand(){}
+    ~QuadCommand(){}
     Vec2 Position; /*8bit*/
     Vec2 UV; /*8bit*/
 } QuadCommand;
 
+
+static std::array<QuadCommand, 4> createQuadCommand(float x, float y, float width, float height) {
+    QuadCommand v0;
+    v0.Position = { x, y};
+    v0.UV = {0, 0};
+    QuadCommand v1;
+    v1.Position = { x + width, y};
+    v1.UV = {1, 0};
+    QuadCommand v2;
+    v2.Position = { x + width, y + height};
+    v2.UV = {1, 1};
+    QuadCommand v3;
+    v3.Position = { x, y + height};
+    v3.UV = {0, 1};
+    return { v0, v1, v2, v3 };
+}
 class GLWindow {
 public:
     GLWindow() {}
@@ -34,6 +51,7 @@ public:
     void create()
     {
         indices = new GLushort[MAX_INDEX_NUMBER];
+        command = new QuadCommand[MAX_QUAD_COMMAND];
         if (!glfwInit())
             return;
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -42,7 +60,7 @@ public:
         #ifdef __APPLE__
             glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
         #endif
-        
+        m_proj = glm::ortho(0.0f, 640.0f, 0.0f, 480.0f, -1.0f, 1.0f);
         GLFWwindow* window = glfwCreateWindow(640, 480, "Wave Simulation", NULL, NULL);
         if (!window)
         {
@@ -83,6 +101,109 @@ public:
         glVertexAttribPointer(0, 2, GL_FLOAT, GL_TRUE, sizeof(QuadCommand), (const void*)offsetof(QuadCommand, Position));
         glEnableVertexAttribArray(1);
         glVertexAttribPointer(1, 2, GL_FLOAT, GL_TRUE, sizeof(QuadCommand), (const void*)offsetof(QuadCommand, UV));
+        
+        glGenBuffers(1, &m_EBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, MAX_INDEX_NUMBER * sizeof(GL_UNSIGNED_SHORT), indices, GL_STATIC_DRAW);
+    }
+    
+    void onUpdate()
+    {
+        int offset = 0;
+        if (m_SpriteList.empty())
+        {
+            Shader* shader = Shader::Create("res/shaders/vertex.shader", "res/shaders/fragment.shader");
+            shader->setUniform1i("u_Texture2D", 0);
+            shader->Bind();
+            shader->setUniform4f("u_Color", 1.0f, 1.f, 1.f, 1.0f);
+            shader->setUniformMat4f("u_MVP", m_proj);
+            for (int i = 0; i != 1000; i++)
+            {
+                Sprite* sprite = new Sprite("res/test.png");
+                sprite->setShaderProgram(shader);
+                sprite->setPosition(100, 100);
+                m_SpriteList.push_back(sprite);
+                memcpy(&command[offset++ * 4], createQuadCommand(sprite->getPosition().x, sprite->getPosition().y, sprite->getTexture2D()->GetWidth(), sprite->getTexture2D()->GetHeight()).data(), sizeof(QuadCommand) * 4);
+            }
+            for (int i = 0; i != 1000; i++)
+            {
+                Sprite* sprite = new Sprite("res/1111_ygfs.png");
+                sprite->setShaderProgram(shader);
+                sprite->setPosition(100, 100);
+                m_SpriteList.push_back(sprite);
+                memcpy(&command[offset++ * 4], createQuadCommand(sprite->getPosition().x, sprite->getPosition().y, sprite->getTexture2D()->GetWidth(), sprite->getTexture2D()->GetHeight()).data(), sizeof(QuadCommand) * 4);
+            }
+            for (int i = 0; i != 1000; i++)
+            {
+                Sprite* sprite = new Sprite("res/test.png");
+                sprite->setShaderProgram(shader);
+                sprite->setPosition(100, 100);
+                m_SpriteList.push_back(sprite);
+                memcpy(&command[offset++ * 4], createQuadCommand(sprite->getPosition().x, sprite->getPosition().y, sprite->getTexture2D()->GetWidth(), sprite->getTexture2D()->GetHeight()).data(), sizeof(QuadCommand) * 4);
+            }
+            unsigned int size = sizeof(QuadCommand) * 4 * m_SpriteList.size();
+            GLCall(glBufferSubData(GL_ARRAY_BUFFER, 0, size, command));
+        }
+        
+        unsigned int drawCount = 0;
+        unsigned int startIndex = 0;
+        (*m_SpriteList.begin())->getShaderProgram()->Bind();
+        (*m_SpriteList.begin())->getTexture2D()->Bind();
+        unsigned int currShader = 0;
+        unsigned int currTextureID = 0;
+        Texture2D* texture = nullptr;
+        Shader* shader = nullptr;
+        for (auto itr : m_SpriteList)
+        {
+            if (!itr->getTexture2D() || !itr->getShaderProgram())
+                continue;
+            texture = itr->getTexture2D();
+            shader = itr->getShaderProgram();
+            if (texture->getRendererID() != currTextureID ||
+                shader->getRandererID() != currShader)
+            {
+                
+                // new draw command
+                currTextureID = texture->getRendererID();
+                currShader = shader->getRandererID();
+                if (drawCount)
+                {
+                    GLCall(glDrawElements(GL_TRIANGLES, 6 * drawCount, GL_UNSIGNED_SHORT, (GLvoid*)((startIndex * sizeof(GLushort)))));
+                    texture->Bind();
+                    shader->Bind();
+                    startIndex += drawCount * 6;
+                    drawCount = 0;
+                }
+                ++drawCount;
+            }
+            else
+            {
+                ++drawCount;
+            }
+        }
+        if (drawCount)
+        {
+            GLCall(glDrawElements(GL_TRIANGLES, 6 * drawCount, GL_UNSIGNED_SHORT, (GLvoid*)((startIndex * sizeof(GLushort)))));
+        }
+    }
+    
+    void render()
+    {
+        if (!glfwWindowShouldClose(m_Window))
+        {
+            GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
+            GLCall(glClear(GL_COLOR_BUFFER_BIT));
+            onUpdate();
+            glfwSwapBuffers(m_Window);
+        }
+        Sprite * spr = nullptr;
+        while (!m_SpriteList.empty())
+        {
+            spr = (*m_SpriteList.begin());
+            m_SpriteList.pop_front();
+            delete spr;
+        }
+            
     }
     
     GLFWwindow* getWindow() const { return m_Window; }
@@ -95,8 +216,12 @@ public:
 private:
     GLFWwindow* m_Window;
     GLushort* indices;
+    QuadCommand* command;
     unsigned int m_VAO;
     unsigned int m_VB;
+    unsigned int m_EBO;
+    std::list<Sprite*> m_SpriteList;
+    glm::mat4 m_proj;
 };
 
 #endif /* GLWindow_hpp */
--- gll/main.cpp
+++ gll/main.cpp
@@ -16,125 +16,11 @@
 #include "Sprite.hpp"
 #include "GLWindow.hpp"
 
-#define MAX_RECT_NUMBER 10000
-#define MAX_QUAD_COMMAND 40000
-#define MAX_INDEX_NUMBER 60000
-const float displayWidth = 640.0f;
-const float displayHeight = 480.0f;
-glm::mat4 proj = glm::ortho(0.0f, displayWidth, 0.0f, displayHeight, -1.0f, 1.0f);
 
-std::list<Sprite*> spriteList;
-QuadCommand command[MAX_QUAD_COMMAND];
 unsigned short offset = 0;
 unsigned int timeDiff = 0;
 unsigned int renderCount = 0;
-static std::array<QuadCommand, 4> createQuadCommand(float x, float y, float width, float height) {
-    QuadCommand v0;
-    v0.Position = { x, y};
-    v0.UV = {0, 0};
-    QuadCommand v1;
-    v1.Position = { x + width, y};
-    v1.UV = {1, 0};
-    QuadCommand v2;
-    v2.Position = { x + width, y + height};
-    v2.UV = {1, 1};
-    QuadCommand v3;
-    v3.Position = { x, y + height};
-    v3.UV = {0, 1};
-    return { v0, v1, v2, v3 };
-}
 
-void render(const std::list<Sprite*>& spriteList)
-{
-    unsigned int drawCount = 0;
-    unsigned int startIndex = 0;
-    (*spriteList.begin())->getShaderProgram()->Bind();
-    (*spriteList.begin())->getTexture2D()->Bind();
-    unsigned int currShader = 0;
-    unsigned int currTextureID = 0;
-    Texture2D* texture = nullptr;
-    Shader* shader = nullptr;
-    for (auto itr : spriteList)
-    {
-        if (!itr->getTexture2D() || !itr->getShaderProgram())
-            continue;
-        texture = itr->getTexture2D();
-        shader = itr->getShaderProgram();
-        if (texture->getRendererID() != currTextureID ||
-            shader->getRandererID() != currShader)
-        {
-            
-            // new draw command
-            currTextureID = texture->getRendererID();
-            currShader = shader->getRandererID();
-            if (drawCount)
-            {
-                GLCall(glDrawElements(GL_TRIANGLES, 6 * drawCount, GL_UNSIGNED_SHORT, (GLvoid*)((startIndex * sizeof(GLushort)))));
-                texture->Bind();
-                shader->Bind();
-                startIndex += drawCount * 6;
-                drawCount = 0;
-            }
-            ++drawCount;
-        }
-        else
-        {
-            ++drawCount;
-        }
-    }
-    if (drawCount)
-    {
-        GLCall(glDrawElements(GL_TRIANGLES, 6 * drawCount, GL_UNSIGNED_SHORT, (GLvoid*)((startIndex * sizeof(GLushort)))));
-    }
-}
-
-void onUpdate(unsigned int diff)
-{
-    if (spriteList.empty())
-    {
-        Shader* shader = Shader::Create("res/shaders/vertex.shader", "res/shaders/fragment.shader");
-        shader->setUniform1i("u_Texture2D", 0);
-        shader->Bind();
-        shader->setUniform4f("u_Color", 1.0f, 1.f, 1.f, 1.0f);
-        shader->setUniformMat4f("u_MVP", proj);
-        for (int i = 0; i != 1000; i++)
-        {
-            Sprite* sprite = new Sprite("res/test.png");
-            sprite->setShaderProgram(shader);
-            sprite->setPosition(100, 100);
-            spriteList.push_back(sprite);
-            memcpy(&command[offset++ * 4], createQuadCommand(sprite->getPosition().x, sprite->getPosition().y, sprite->getTexture2D()->GetWidth(), sprite->getTexture2D()->GetHeight()).data(), sizeof(QuadCommand) * 4);
-        }
-        for (int i = 0; i != 1000; i++)
-        {
-            Sprite* sprite = new Sprite("res/1111_ygfs.png");
-            sprite->setShaderProgram(shader);
-            sprite->setPosition(100, 100);
-            spriteList.push_back(sprite);
-            memcpy(&command[offset++ * 4], createQuadCommand(sprite->getPosition().x, sprite->getPosition().y, sprite->getTexture2D()->GetWidth(), sprite->getTexture2D()->GetHeight()).data(), sizeof(QuadCommand) * 4);
-        }
-        for (int i = 0; i != 1000; i++)
-        {
-            Sprite* sprite = new Sprite("res/test.png");
-            sprite->setShaderProgram(shader);
-            sprite->setPosition(100, 100);
-            spriteList.push_back(sprite);
-            memcpy(&command[offset++ * 4], createQuadCommand(sprite->getPosition().x, sprite->getPosition().y, sprite->getTexture2D()->GetWidth(), sprite->getTexture2D()->GetHeight()).data(), sizeof(QuadCommand) * 4);
-        }
-        unsigned int size = sizeof(QuadCommand) * 4 * spriteList.size();
-        GLCall(glBufferSubData(GL_ARRAY_BUFFER, 0, size, command));
-    }
-    render(spriteList);
-    renderCount++;
-    if (timeDiff + diff > 1000)
-    {
-        timeDiff = 0;
-        printf("fps: %d\n", renderCount);
-        renderCount = 0;
-    }
-    else
-        timeDiff += diff;
-}
 
 
 int main(int argc, const char * argv[]) {
@@ -145,34 +31,20 @@ int main(int argc, const char * argv[]) {
     GLFWwindow* window = GLwindow->getWindow();
     GLwindow->setupContext();
 
-    auto VAO = GLwindow->getVAO();
-    auto indices = GLwindow->getIndices();
-    IndexBuffer ib(indices, MAX_INDEX_NUMBER);
-    ib.Bind();
     
     auto begin = std::chrono::high_resolution_clock::now();
-    while (!glfwWindowShouldClose(window))
+    while (1)
     {
         // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
         glfwPollEvents();
-        GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
-        GLCall(glClear(GL_COLOR_BUFFER_BIT));
-        glBindVertexArray(VAO);
         auto now = std::chrono::high_resolution_clock::now();
-        onUpdate(std::chrono::duration_cast<std::chrono::milliseconds>(now - begin).count());
+//        onUpdate(std::chrono::duration_cast<std::chrono::milliseconds>(now - begin).count());
         // Swap the screen buffers
+        GLwindow->render();
         begin = now;
-        glfwSwapBuffers(window);
+        // glfwSwapBuffers(window);
     }
-    
-    Sprite * spr = nullptr;
-    while (!spriteList.empty())
-    {
-        spr = (*spriteList.begin());
-        spriteList.pop_front();
-        delete spr;
-    }
-    
+
     glfwTerminate();
     return 0;
 }
